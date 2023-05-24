a1bb262514de1860622678181ac1ef801eddcc48
Ian Chege <ianchege001@gmail.com>
2023-05-24 19:52:25 +0300

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: 'Promptopia',
+    description: 'Discover & Share AI prompts'
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient' />
+            </div>
+        </body>
+        <main className='app'>
+            {children}
+        </main>
+    </html>
+  )
+}
+
+export default RootLayout;
